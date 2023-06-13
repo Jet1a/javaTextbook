1965ecef7a2f869fa1017c8ba5741e3a5c46860b
Jet1a <jettaphoom@gmail.com>
2023-06-13 16:51:57 +0700

    javaTextbook

---
+++ textbook/Display.java
@@ -0,0 +1,20 @@
+package textbook;
+
+public class Display{
+    public static void main(String[] args) {
+
+        System.out.println("Hello Test");
+        System.out.println("Welcome to hell");
+        //output จะเอ๋ตัวเอง
+        /*
+        Many line comment
+        Hello Test
+        Welcome to hell
+        */ 
+
+        System.out.print("Hello Test");
+        System.out.println("Welcome to hell");
+        //output 
+        // Hello TestWelcome to hell
+    }
+}
