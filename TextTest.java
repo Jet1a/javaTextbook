ec014298331a958078f3ea446c1d8fcc2d76520e
Jet1a <jettaphoom@gmail.com>
2023-06-14 16:08:39 +0700

    javaTextbook

--- TextTest.java
+++ TextTest.java
@@ -7,12 +7,20 @@ public class TextTest {
      char mix = String
      */
     public static void main(String[] args) {
-        String a = "100", b = "DEF";
+        String a = "100", b = "3.14";
         int c;
         // String => int
-        c = Integer.parseInt(a);
+        c = Integer.parseInt(a); //can replace a as "100"
         c = c + 50;
         System.out.println(a);
         System.out.println(c);
+        // String => Double
+        double d = Double.parseDouble(b);
+        d = d + 3.55;
+        System.out.println(d);
+        // Integer => String
+        int num1 = 100;
+        String age = String.valueOf(num1); //เช็คค่าที่โยนมาเป็นตัวเลขหรืออื่นๆ
+        System.out.println(age);
     }
 }
