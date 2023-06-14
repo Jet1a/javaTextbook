f78c9cd25b830f9dc0cfdc6493bafcaff7ce70b9
Jet1a <jettaphoom@gmail.com>
2023-06-14 09:43:04 +0700

    javaTextbook

--- textbook/Display.java
+++ textbook/Display.java
@@ -1,6 +1,25 @@
 package textbook;
+/*
+--- Variable ---
+
+ชื่อคลาส ชื่อไฟล์ ต้องเหมือนกัน
+ชื่อคลาสต้องเป็นตัวพิมพ์ใหญ่ MyClass,Hello
+ตัวแปรต้องเป็นตัวพิมพ์เล็ก age,number
+ค่าคงที่ ต้องพิมพ์ใหญ่หมด SIZE,WEIGHT
+
+--- Data Type ---
+
+--> Primitive Data Types
+> Integer (byte,short,int,long) 8 > 16 > 32 > 64 bit
+> Floating-Point (float,double) 32 > 64 ((Decimal))
+> Character (char) ได้แค่หนึ่งตัวอักษร 16 bit
+> Boolean (boolean) 8 bit
+--> Non-primitive Data Types
+
+*/
 
 public class Display{
+    
     public static void main(String[] args) {
 
         System.out.println("Hello Test");
---
+++ textbook/Variable.java
@@ -0,0 +1,36 @@
+package textbook;
+/* การสร้างตัวแปร
+ประกอบด้วยตัวเลจ ตัวอักษร เครื่องหมาย
+อักษรแรกห้างขึ้นต้นด้วยตัวเลขกับอักษรพิเศษยกเว้น _
+ห้ามซ้ำคำสงวน
+Case Sensitive
+*/
+public class Variable {
+    public static void main(String[] args) {
+        //นิยามตัวแปรไม่ระยุค่าเริ่มต้น
+        int number1;
+        float number2;
+        double number3;
+        boolean status;
+        char alphabet;
+        //นิยามตัวแปรระยุค่าเริ่มต้น
+        int number = 10;
+        int num2 = 20;
+        num2 = 69;
+        System.out.println("Your number is: "+number);
+        System.out.println(num2);
+        float count = 20.5f; //float
+        char d = 'A';
+        boolean s=true;
+        //นิยามหลายบรรทัด
+        int a=0,b=1;
+        //นิยามค่าคงที่ เปลี่ยนแปลงไม่ได้ ตัวพิมพ์ใหญ่ทั้งหมด!!!!
+        final int NS = 200;
+        //Global,Local Variable
+        {
+            int c = 300; //ทำงานแค่ในขอบเขตนี้ Local
+            System.out.println(c);
+            System.out.println("A = " + a);
+        }
+    }
+}
