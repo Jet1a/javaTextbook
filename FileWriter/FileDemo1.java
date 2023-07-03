272af99166e44f88a58418c40f6d537f731347c8
Jet1a <jettaphoom@gmail.com>
2023-07-03 10:40:42 +0700

    javaTextbook

--- FileWriter/FileDemo1.java
+++ FileWriter/FileDemo1.java
@@ -1,12 +1,17 @@
+package FileWriter;
 import java.io.*;
-public class FileDemo1 {
+    public class FileDemo1 {
     public static void main(String[] args) {
-        File f=new File("Demo.txt");
+        //File f=new File("Demo.txt");
         try{
-            FileWriter writer = new FileWriter(new File("Score.txt"));
-            writer.write("Hello World");
-            writer.write("Are you Ok");
-            writer.close();
+            FileWriter writer = new FileWriter(new File("Introduction.txt"));
+            BufferedWriter bw = new BufferedWriter(writer);
+            bw.write("Hello Chat\n");
+            bw.write("Am Broke");
+            // writer.write("Hello World");
+            // writer.write("Are you Ok");
+            //writer.close();
+            bw.close();
             System.out.println("เขียนไฟล์สำเร็จแล้ว!");
             
         } catch(Exception e){
--- FileWriter/FileDemo2.java
+++ FileWriter/FileDemo2.java
@@ -1,12 +1,17 @@
+package FileWriter;
 import java.io.*;
 class FileDemo2{
     public static void main(String[] args) {
         try {
             FileReader reader = new FileReader(new File("Score.txt"));
-            int data;
-            while((data=reader.read()) !=-1){
-                System.out.printf("%c\n",data);
+            BufferedReader br = new BufferedReader(reader);
+            String message = "";
+            while((message = br.readLine())!=null){
+                System.out.println(message);
             }
+            // int data;
+            // while((data=reader.read()) !=-1){
+            //     System.out.printf("%c\n",data);
         } catch (Exception e) {
             e.printStackTrace();
         }
