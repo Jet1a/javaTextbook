34f280168c569d5f95833af94ed41064c6a47377
jetta <jettaphoom@gmail.com>
2023-07-13 16:10:03 +0700

    javaTextbook

--- Project/Calculator.java
+++ Project/Calculator.java
@@ -31,6 +31,6 @@ public class Calculator {
         }else{
             System.out.println("Invalid Selection!");
         }
-        System.out.println("----- End of Program -----");
+        System.out.println("----- End of Program! -----");
     }
 }
